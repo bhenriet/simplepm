a479badc89cd01698038b55961168c218db46ac4
Benjamin Henriet <benjamin.henriet@gmail.com>
2013-02-16 14:20:25 +0100

    simple file parser

---
+++ simplepm.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import sys, getopt
+import os
+
+def parse(filePath):
+   file = open(filePath, 'r')
+   for line in file:
+      print line
+   file.close()   
+
+def main(argv):
+   inputfile = ''
+   outputfile = ''
+   try:
+      opts, args = getopt.getopt(argv,"hi:",["ifile="])
+   except getopt.GetoptError:
+      print 'simplepm.py -i <inputfile>'
+      sys.exit(2)
+   for opt, arg in opts:
+      if opt == '-h':
+         print 'simplepm.py -i <inputfile>'
+         sys.exit()
+      elif opt in ("-i", "--ifile"):
+         inputPath = arg
+   parse(inputPath)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
